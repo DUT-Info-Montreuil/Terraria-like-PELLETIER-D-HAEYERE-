f7d097351440331c0f84e4066b30cd6e7c3d9c87
THEfireZz <maximedhaeyere@gmail.com>
2022-05-12 12:30:43 +0200

    Fichier Json utilisable

--- src/Terraria/modele/Environnement.java
+++ src/Terraria/modele/Environnement.java
@@ -1,8 +1,30 @@
 package Terraria.modele;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.*;
+
 
 public class Environnement {
-    private  int world[][];
+    private  int world[];
+    public Environnement() {
+        JSONParser parser = new JSONParser();
+        File resourceFile = new File("src/Terraria/vue/terrain.json");
+        try (Reader reader = new FileReader(resourceFile)) {
 
+            JSONObject jsonObject = (JSONObject) parser.parse(reader);
+            System.out.println(jsonObject);
+            System.out.println("-----");
+            JSONObject name =  (JSONObject) jsonObject.get("conf");
+            JSONObject material = (JSONObject) name.get("material");
+            String test = (String) material.get("0") ;
+            System.out.println(test);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        this.world = new int[4096];
+    }
 }
--- src/Terraria/modele/Lancement.java
+++ src/Terraria/modele/Lancement.java
@@ -3,6 +3,6 @@ package Terraria.modele;
 public class Lancement {
 
 	public static void main(String [] args){
-
+		Environnement e1 = new Environnement();
 	}
 }
