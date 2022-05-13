9e803ead80b6195391329e6b67bd032342e4ebba
THEfireZz <maximedhaeyere@gmail.com>
2022-05-13 10:33:19 +0200

    Json ouvrable format Tiled Tiled

--- src/Terraria/modele/Environnement.java
+++ src/Terraria/modele/Environnement.java
@@ -4,27 +4,31 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 
 public class Environnement {
-    private  int world[];
+    private JSONObject layers;
+    private  ArrayList <Integer> world;
+
     public Environnement() {
         JSONParser parser = new JSONParser();
         File resourceFile = new File("src/Terraria/vue/terrain.json");
         try (Reader reader = new FileReader(resourceFile)) {
 
             JSONObject jsonObject = (JSONObject) parser.parse(reader);
-            System.out.println(jsonObject);
-            System.out.println("-----");
-            JSONObject name =  (JSONObject) jsonObject.get("conf");
-            JSONObject material = (JSONObject) name.get("material");
-            String test = (String) material.get("0") ;
-            System.out.println(test);
+            JSONArray conf = (JSONArray) jsonObject.get("layers");
+            Iterator<JSONObject> confIterator= conf.iterator();
+            this.layers = confIterator.next();
+            this.world = (ArrayList<Integer>) layers.get("data");
+            System.out.println(world);
+
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        this.world = new int[4096];
+
     }
 }
