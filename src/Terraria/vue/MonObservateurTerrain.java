25f96734f5837e954dd512f18a0b2d3983237f7c
THEfireZz <maximedhaeyere@gmail.com>
2022-05-23 14:46:06 +0200

    modification json + Imageview ajouté à l'event Handler

--- src/Terraria/vue/MonObservateurTerrain.java
+++ src/Terraria/vue/MonObservateurTerrain.java
@@ -1,8 +1,10 @@
 package Terraria.vue;
 
 import javafx.collections.ListChangeListener;
+import javafx.event.EventHandler;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 import java.util.HashMap;
@@ -11,10 +13,12 @@ public class MonObservateurTerrain implements ListChangeListener<Integer> {
 
     private Pane paneJeu;
     private HashMap mapImage;
-    public MonObservateurTerrain(Pane paneJeu,HashMap mapImage){
+    private EventHandler<MouseEvent> eventHandler;
+    public MonObservateurTerrain(Pane paneJeu,HashMap mapImage,EventHandler<MouseEvent>eventHandler){
         super();
         this.paneJeu=paneJeu;
         this.mapImage=mapImage;
+        this.eventHandler=eventHandler;
     }
     @Override
     public void onChanged(javafx.collections.ListChangeListener.Change<? extends Integer> c) {
@@ -27,13 +31,13 @@ public class MonObservateurTerrain implements ListChangeListener<Integer> {
                 ImageView imageSupr = (ImageView) paneJeu.lookup("#"+c.getFrom());
                 testIV.setX(imageSupr.getX());
                 testIV.setY(imageSupr.getY());
+                testIV.setId(imageSupr.getId());
                 paneJeu.getChildren().remove(imageSupr);
                 paneJeu.getChildren().add(testIV);
                 System.out.println("testremove");
                 System.out.println(c.getFrom());
-
-
                 ImageView newIV = new ImageView((Image) mapImage.get(0));
+                testIV.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
 
             }
 //            for (Integer nouveau:c.getAddedSubList()
