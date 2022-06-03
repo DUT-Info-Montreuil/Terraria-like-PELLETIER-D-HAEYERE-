07aa6157dfefeeb2f7d780e46009cbc9f56e4b7d
THEfireZz <maximedhaeyere@gmail.com>
2022-06-03 16:04:51 +0200

    TAB ajouter au KeyHandler pour l'ouverture de l'inventraire

--- src/Terraria/controleur/Controleur.java
+++ src/Terraria/controleur/Controleur.java
@@ -5,6 +5,7 @@ import Terraria.modele.*;
 import Terraria.vue.MonObservateurListActeur;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -12,7 +13,6 @@ import javafx.scene.ParallelCamera;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -38,7 +38,7 @@ public class Controleur implements Initializable {
     public final int sprit_largeur = 16;
 
     private EventHandler<MouseEvent> eventHandler;
-    private KeyHandler keyHandler ;
+    private KeyHandler keyHandler;
 
 
     @Override
@@ -49,14 +49,14 @@ public class Controleur implements Initializable {
 
 
         Pioche piocheDep = new Pioche("piocheDep", 250, 10, e1);
-        ItemBlock blockLave = new ItemBlock(20,"blockLave",e1,3);
-        this.e1.getListActeur().addListener(new MonObservateurListActeur(e1,pane));
+        ItemBlock blockLave = new ItemBlock(20, "blockLave", e1, 3);
+        this.e1.getListActeur().addListener(new MonObservateurListActeur(e1, pane));
         Joueur hero = new Joueur(20, 5, 50, -40, e1, "hero", new HitBox(50, 30, 24, 14, true), piocheDep);
-        Zombie z = new Zombie(20 , 5 ,30 , 30, e1 , "Zombie" , new HitBox(50 , 30, 28 , 16 , true) );
+        Zombie z = new Zombie(20, 5, 30, 30, e1, "Zombie", new HitBox(50, 30, 28, 16, true));
         e1.addActeur(hero);
         e1.addActeur(z);
         e1.addEnnemi(z);
-       
+
 
 //
 
@@ -96,9 +96,20 @@ public class Controleur implements Initializable {
                     //System.out.println(idDansLeTerrain+"id avant le if");
                     //System.out.println(typeDeLaTile+"valeur à lid avant le if");
                     //System.out.println(e1.getJoueur1().getItemEquipe().cielEstModifiable(typeDeLaTile));
-                    if (e1.getJoueur1().getItemEquipe().cielEstModifiable(typeDeLaTile)){
+                    if (e1.getJoueur1().getItemEquipe().cielEstModifiable(typeDeLaTile)) {
                         System.out.println("est ce que c'est du ciel ?");
                         modifTerrain(mapLienIdImage, imageClicked, Integer.parseInt(imageClicked.getId()));
+                        for (Item i : e1.getJoueur1().getInventaire()
+                        ) {
+                            if (i instanceof ItemBlock) {
+                                ItemBlock block = (ItemBlock) i;
+                                if (block.getCode() == typeDeLaTile) {
+                                    i.quantiteEnPlus();
+                                }
+                            }
+                        }
+
+
                     }
 
 //                        e1.terrainToString();
@@ -122,9 +133,8 @@ public class Controleur implements Initializable {
 
         e1.loadLayers();
 
-        afficheMap(e1,mapLienIdImage);
-        afficherColision(allBlock ,hero ,e1.getListActeur() ,  true);
-
+        afficheMap(e1, mapLienIdImage);
+        afficherColision(e1.getAllBlock(), hero, e1.getListActeur(), false);
 
 
         //ajoutSprite(hero);
@@ -135,20 +145,17 @@ public class Controleur implements Initializable {
         //pane.getScene().getCamera().layoutYProperty().bind(hero.getYProprety().subtract(pane.getScene().getHeight() / 2));
 
 
-
-
         keyHandler = new KeyHandler(pane);
         keyHandler.start();
-
 
 
         //Registering the event filter
 
 
-        for(Acteur a : e1.getListActeur()){
-            pane.lookup("#"+a.getId()).toFront();
+        for (Acteur a : e1.getListActeur()) {
+            pane.lookup("#" + a.getId()).toFront();
         }
-        (pane.lookup("#"+e1.getJoueur1().getId())).toFront();
+        (pane.lookup("#" + e1.getJoueur1().getId())).toFront();
 
         launchTimeLine();
         timeline.setCycleCount(timeline.INDEFINITE);
@@ -164,22 +171,22 @@ public class Controleur implements Initializable {
         timeline = new Timeline(new KeyFrame(Duration.millis(32.66), actionEvent -> {
 
 
-
-            if (keyHandler.isLeftPressed()){
+            if (keyHandler.isLeftPressed()) {
                 e1.getJoueur1().setDirection(-1);
-            } if (keyHandler.isRightPressed()){
+            }
+            if (keyHandler.isRightPressed()) {
                 e1.getJoueur1().setDirection(1);
-            } if (keyHandler.isUpPressed()){
+            }
+            if (keyHandler.isUpPressed()) {
                 if (!e1.getJoueur1().isJumping()) {
                     e1.getJoueur1().saute();
                 }
             }
-            if (e1.getJoueur1().getDirection() == -1 && !keyHandler.isLeftPressed()){
+            if (e1.getJoueur1().getDirection() == -1 && !keyHandler.isLeftPressed()) {
                 e1.getJoueur1().setDirection(0);
-            }else if (e1.getJoueur1().getDirection() == 1 && !keyHandler.isRightPressed()){
+            } else if (e1.getJoueur1().getDirection() == 1 && !keyHandler.isRightPressed()) {
                 e1.getJoueur1().setDirection(0);
             }
-
 
 
             if (e1.getJoueur1().getDirection() == 1 && e1.getJoueur1().collideGaucheDroite(allBlock) != 1) {
@@ -188,15 +195,15 @@ public class Controleur implements Initializable {
                 e1.getJoueur1().seDeplace();
             }
 
-                for(Acteur a : e1.getListActeur()){
-                    a.collideHautBas(allBlock);
-                    a.gravite();
-                }
+            for (Acteur a : e1.getListActeur()) {
+                a.collideHautBas(allBlock);
+                a.gravite();
+            }
 
 
-                for(Ennemi e : e1.getListEnnemi()){
-                    e.seDeplace(e1.getJoueur1());
-                }
+            for (Ennemi e : e1.getListEnnemi()) {
+                e.seDeplace(e1.getJoueur1());
+            }
 
         }));
 
@@ -248,7 +255,6 @@ public class Controleur implements Initializable {
                         imageView.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
 
                         imageView.toBack();
-
 
 
                         pane.getChildren().add(imageView);
@@ -319,16 +325,12 @@ public class Controleur implements Initializable {
     }*/
 
 
-
-
-
-
-    public void afficherColision(ArrayList<Block> blocks , Acteur a  , ObservableList<Acteur>allActeur, boolean affiche ){
-        if (affiche){
+    public void afficherColision(ArrayList<Block> blocks, Acteur a, ObservableList<Acteur> allActeur, boolean affiche) {
+        if (affiche) {
 //            System.out.println(blocks.size());
 
-            for (Block b: blocks) {
-                Rectangle r = new Rectangle(b.getBoxX().intValue() , b.getBoxY().intValue() , b.getTile().getWidth(), b.getTile().getHauteur()) ;
+            for (Block b : blocks) {
+                Rectangle r = new Rectangle(b.getBoxX().intValue(), b.getBoxY().intValue(), b.getTile().getWidth(), b.getTile().getHauteur());
 
                 r.setFill(Color.TRANSPARENT);
                 r.setStroke(Color.RED);
@@ -337,13 +339,13 @@ public class Controleur implements Initializable {
                 pane.getChildren().add(r);
 
             }
-            for (Acteur a1: allActeur) {
-                Rectangle r = new Rectangle(a1.getBox().getX().intValue() , a1.getBox().getY().intValue() , a1.getBox().getWidth(), a1.getBox().getHeight()) ;
+            for (Acteur a1 : allActeur) {
+                Rectangle r = new Rectangle(a1.getBox().getX().intValue(), a1.getBox().getY().intValue(), a1.getBox().getWidth(), a1.getBox().getHeight());
                 r.setFill(Color.TRANSPARENT);
                 r.setStroke(Color.YELLOW);
                 r.xProperty().bind(a1.getBox().getX());
                 r.yProperty().bind(a1.getBox().getY());
-                pane.getChildren().add(r) ;
+                pane.getChildren().add(r);
 
             }
 
